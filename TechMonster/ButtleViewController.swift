d5da6bc2daee77e2ca9200f4940acbb99265335d
losiz17 <riokam.11.12@docomo.ne.jp>
2018-04-12 22:25:25 +0900

    delate 素材

--- TechMonster/ButtleViewController.swift
+++ TechMonster/ButtleViewController.swift
@@ -9,12 +9,104 @@
 import UIKit
 
 class ButtleViewController: UIViewController {
-
+    var enemyAttackTimer: Timer!
+    var enemy: Enemy!
+    var player: Player!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+    @IBOutlet var attackButton: UIButton!
+    
+    @IBOutlet var enemyImageView: UIImageView!
+    @IBOutlet var playerImageView: UIImageView!
+    
+    @IBOutlet var enemyHPBar: UIProgressView!
+    @IBOutlet var playerHPBar: UIProgressView!
+    
+    @IBOutlet var enemyNameLabel: UILabel!
+    @IBOutlet var playerNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        enemyHPBar.transform = CGAffineTransform(scaleX: 1.0, y:4.0)
+        playerHPBar.transform = CGAffineTransform(scaleX: 1.0, y:4.0)
+        
+        playerNameLabel.text = player.name
+        playerImageView.image = player.image
+        playerHPBar.progress = player.currentHP / player.maxHP
+        
+        startBattle()
 
         // Do any additional setup after loading the view.
     }
+    
+    func startBattle() {
+        TechDraUtil.playBGM(fileName: "BGM_battle001")
+        
+        enemy = Enemy()
+        //敵のデータセット
+        enemyNameLabel.text = enemy.name
+        enemyImageView.image = enemy.image
+        enemyHPBar.progress = enemy.currentHP / enemy.maxHP
+        //攻撃ボタン表示
+        attackButton.isHidden = false
+        //敵の自動攻撃
+        enemyAttackTimer = Timer.scheduledTimer(timeInterval: enemy.attackInterval, target: self,
+                                                selector: #selector(self.enemyAttack), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func playerAttack() {
+        TechDraUtil.animateDamage(enemyImageView)
+        TechDraUtil.playSE(fileName: "SE_attack")
+        //HP更新
+        enemy.currentHP = enemy.currentHP - player.attackPower
+        enemyHPBar.setProgress(enemy.currentHP / enemy.maxHP, animated: true)
+        //敵の敗北
+        if enemy.currentHP < 0 {
+            TechDraUtil.animateVanish(enemyImageView)
+            finishBattle(winPlayer: true)
+        }
+    }
+    
+    func finishBattle(winPlayer: Bool) {
+        enemyAttackTimer.invalidate()
+        TechDraUtil.stopBGM()
+        //攻撃ボタンおす
+        attackButton.isHidden = true
+        //敵の自動攻撃とめる
+        enemyAttackTimer.invalidate()
+        //アラートを表示
+        let finishedMessage: String
+        if winPlayer == true {
+            TechDraUtil.playSE(fileName: "SE_fanface")
+            finishedMessage = "プレイヤーの勝利！！"
+        }else{
+            TechDraUtil.playSE(fileName: "SE_gameover")
+            finishedMessage = "プレイヤーの敗北..."
+        }
+        let alert = UIAlertController(title: "バトル終了！",message: finishedMessage, preferredStyle:UIAlertControllerStyle.alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in
+            //OKをおすとモーダルを消して、Lobbyへ戻る
+            self.dismiss(animated:  true, completion: nil)
+        })
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func enemyAttack(){
+        TechDraUtil.animateDamage(playerImageView)
+        TechDraUtil.playSE(fileName: "SE_attack")
+        //HPの更新
+        player.currentHP = player.currentHP - enemy.attackPower
+        playerHPBar.setProgress(player.currentHP / player.maxHP, animated: true)
+        //プレイヤーの敗北
+        if player.currentHP < 0 {
+            TechDraUtil.animateVanish(playerImageView)
+            finishBattle(winPlayer: false)
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- TechMonster/LobbyViewController.swift
+++ TechMonster/LobbyViewController.swift
@@ -9,11 +9,68 @@
 import UIKit
 
 class LobbyViewController: UIViewController {
+    var maxStamina: Float = 100
+    var stamina: Float = 100
+    var player: Player = Player()
+    var staminaTimer: Timer!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaBar: UIProgressView!
+    @IBOutlet var levelLabel: UILabel!
+    
+    @IBAction func startButtle() {
+        if stamina >= 20 {
+            stamina = stamina - 20
+            staminaBar.progress = stamina / maxStamina
+            performSegue(withIdentifier: "startBattle", sender: nil)
+        }else{
+            let alert = UIAlertController(title: "スタミナ不足", message: "スタミナが20以上必要です",
+                                          preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startBattle" {
+            let battleVC = segue.destination as! BattleViewController
+            player.currentHP = player.maxHP
+            battleVC.player = player
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //ステータスバーを縦に拡大
+        staminaBar.transform = CGAffineTransform(scaleX: 1.0, y:4.0)
+        //プレイヤーのデータをセット
+        nameLabel.text = player.name
+        levelLabel.text = String(format: "Lv. %d", player.level)
+        //スタミナを起動時に最大
+        stamina = maxStamina
+        staminaBar.progress = stamina / maxStamina
+        staminaTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:
+            #selector(self.cureStamina), userInfo: nil, repeats: true)
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        TechDraUtil.playBGM(fileName: "lobby")
+    }
+    
+    @objc func cureStamina(){
+        if stamina < maxStamina {
+            stamina = min(stamina + 1, maxStamina)
+            staminaBar.progress = stamina / maxStamina
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        TechDraUtil.stopBGM()
     }
 
     override func didReceiveMemoryWarning() {
